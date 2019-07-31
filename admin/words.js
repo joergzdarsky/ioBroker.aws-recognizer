cedd16279966dcf3c88dead7f910acf321e4be32
joergzdarsky <joerg.zdarsky@gmx.de>
2019-07-31 08:53:59 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"aws-recognizer adapter settings": {
+		"en": "Adapter settings for aws-recognizer",
+		"de": "Adaptereinstellungen für aws-recognizer",
+		"ru": "Настройки адаптера для aws-recognizer",
+		"pt": "Configurações do adaptador para aws-recognizer",
+		"nl": "Adapterinstellingen voor aws-recognizer",
+		"fr": "Paramètres d'adaptateur pour aws-recognizer",
+		"it": "Impostazioni dell'adattatore per aws-recognizer",
+		"es": "Ajustes del adaptador para aws-recognizer",
+		"pl": "Ustawienia adaptera dla aws-recognizer",
+		"zh-cn": "aws-recognizer的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
